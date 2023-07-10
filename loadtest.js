e43ab9435dd4abdb8a0c3b55070ca3cc90e84864
julypenguin <kaopenguin@gmail.com>
2023-07-10 13:36:37 +0800

    loadtest 初版

---
+++ loadtest.js
@@ -0,0 +1,67 @@
+require('isomorphic-fetch')
+const { apiDomain } = require("./config.json");
+
+function parseCookies(response) {
+    const raw = response.headers.raw()['set-cookie'];
+    return raw.map((entry) => {
+        const parts = entry.split(';');
+        const cookiePart = parts[0];
+        return cookiePart;
+    }).join(';');
+}
+
+function loading_test({ url = '/', method = 'GET', repeat }) {
+    let total_time = 0 // 同一支 api 全部結束後的總回傳時間
+    let fastest_time = 0 // 同一支 api 全部結束後，最快回傳的時間是多少
+    let slowest_time = 0 // 同一支 api 全部結束後，最慢回傳的時間是多少
+    let avg_time = 0 // 同一支 api 全部結束後，計算平均回傳時間是多少
+    let time_list = [] // 同一支 api 全部結束後，把所有 api 回傳的時間用陣列紀錄
+    let resIdx = 0 // 這支 api 已經進行了幾次
+    let cookie = ''
+    return {
+        total_time,
+        fastest_time,
+        slowest_time,
+        avg_time,
+        time_list,
+        resIdx,
+        cookie,
+        test(option, cookie) {
+            if (!url) return new Promise(resolve => resolve)
+            const start_time = Date.now()
+            const headers = option.headers || {}
+            const body = option.body || {}
+
+            return fetch(`${apiDomain}${url}`, {
+                method,
+                ...option,
+                headers: {
+                    "Content-Type": "application/json",
+                    ...headers,
+                    cookie,
+                },
+                body: method.toUpperCase() === 'GET' ? undefined : JSON.stringify(body)
+            })
+                .then(res => {
+                    if (!cookie) {
+                        this.cookie = parseCookies(res)
+                    }
+                    const end_time = Date.now()
+                    const res_time = end_time - start_time
+
+                    this.total_time = this.total_time + res_time
+                    this.resIdx = this.resIdx + 1
+                    this.fastest_time = res_time < this.fastest_time || !this.fastest_time ? res_time : this.fastest_time
+                    this.slowest_time = res_time > this.slowest_time ? res_time : this.slowest_time
+                    this.time_list.push(res_time)
+
+                    if (repeat === this.resIdx) {
+                        this.avg_time = this.total_time / this.resIdx
+                    }
+                    return res_time
+                })
+        }
+    }
+}
+
+module.exports = loading_test
---
+++ sample.js
@@ -0,0 +1,50 @@
+const loading_test = require("./loadtest");
+const { exec } = require("child_process");
+
+const repeat = 5
+
+const login = loading_test({ url: '/core/login', method: 'POST', repeat })
+const getAnnList = loading_test({ url: '/core/announcement?enable_state=1&is_read=-1', repeat })
+
+const total_res_time_list = [] // 陣列紀錄所有下面一系列 api 分別總回傳時間
+let fastest_total_res_time = 0 // 下面一系列 api 總回傳時間最快的
+let slowest_total_res_time = 0 // 下面一系列 api 總回傳時間最慢的
+
+for (let i = 1; i <= repeat; i++) {
+    let total_res_time = 0 // 下面一系列 api 總回傳時間
+    login
+        .test({
+            body: {
+                account: '',
+                ao_sid: '',
+                password: ''
+            }
+        })
+        .then((res_time) => {
+            total_res_time = total_res_time + res_time
+            return getAnnList.test({}, login.cookie)
+        })
+        .then((res_time) => {
+            total_res_time = total_res_time + res_time
+            total_res_time_list.push(total_res_time)
+
+            fastest_total_res_time = total_res_time < fastest_total_res_time || !fastest_total_res_time ? total_res_time : fastest_total_res_time
+            slowest_total_res_time = total_res_time > slowest_total_res_time ? total_res_time : slowest_total_res_time
+
+            if (total_res_time_list.length === repeat) {
+                const avg_total_res_time = total_res_time_list.reduce((acc, time) => acc + time, 0) / repeat
+                exec(`echo ${JSON.stringify({
+                    login: { ...login, time_list: [] },
+                    getAnnList: { ...getAnnList, time_list: [] },
+                    fastest_total_res_time,
+                    slowest_total_res_time,
+                    avg_total_res_time,
+                })} > test.json`)
+                exec(`echo ${JSON.stringify({
+                    login: login.time_list,
+                    getAnnList: getAnnList.time_list,
+                    total_res_time_list,
+                })} > test_row.json`)
+            }
+        })
+}
