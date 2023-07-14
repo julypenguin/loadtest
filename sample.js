e3677bc6e35bca0f8f0ec6bd6c4d7a62ebae2847
julypenguin <kaopenguin@gmail.com>
2023-07-14 09:29:34 +0800

    優化 - 新增設定判斷成功或失敗的參數

--- sample.js
+++ sample.js
@@ -1,9 +1,9 @@
-const loadtest = require("./loadtest");
-const { exec } = require("child_process");
+const loadtest = require("./lib/loadtest");
+const fs = require('fs');
 
-const repeat = 5
+const repeat = 1
 
-const login = loadtest({ url: '/core/login', method: 'POST', repeat })
+const login = loadtest({ url: '/core/login', method: 'POST', repeat, succResult: { result: true } })
 const getAnnList = loadtest({ url: '/core/announcement?enable_state=1&is_read=-1', repeat })
 
 const total_res_time_list = [] // 陣列紀錄所有下面一系列 api 分別總回傳時間
@@ -18,11 +18,15 @@ for (let i = 1; i <= repeat; i++) {
                 account: ``,
                 ao_sid: '',
                 password: ''
-            }
+            },
         })
         .then((res_time) => {
             total_res_time = total_res_time + res_time
-            return getAnnList.test({ page: i }, login.cookie)
+            return getAnnList.test({
+                headers: {
+                    page: i,
+                }
+            }, login.cookie)
         })
         .then((res_time) => {
             total_res_time = total_res_time + res_time
@@ -33,18 +37,36 @@ for (let i = 1; i <= repeat; i++) {
 
             if (total_res_time_list.length === repeat) {
                 const avg_total_res_time = total_res_time_list.reduce((acc, time) => acc + time, 0) / repeat
-                exec(`echo ${JSON.stringify({
-                    login: { ...login, time_list: [] },
-                    getAnnList: { ...getAnnList, time_list: [] },
-                    fastest_total_res_time,
-                    slowest_total_res_time,
-                    avg_total_res_time,
-                })} > test.json`)
-                exec(`echo ${JSON.stringify({
-                    login: login.time_list,
-                    getAnnList: getAnnList.time_list,
-                    total_res_time_list,
-                })} > test_row.json`)
+                try {
+                    fs.writeFileSync('test.json', JSON.stringify({
+                        login: { ...login, time_list: [], error_list: [] },
+                        getAnnList: { ...getAnnList, time_list: [], error_list: [] },
+                        fastest_total_res_time,
+                        slowest_total_res_time,
+                        avg_total_res_time,
+                    }, null, "\t"));
+                } catch (err) {
+                    console.error(err);
+                }
+
+                try {
+                    fs.writeFileSync('test_row.json', JSON.stringify({
+                        login: {
+                            time_list: login.time_list,
+                            error_list: login.error_list,
+                        },
+                        getAnnList: {
+                            time_list: getAnnList.time_list,
+                            error_list: getAnnList.error_list,
+                        },
+                        total_res_time_list,
+                    }, null, "\t"));
+                } catch (err) {
+                    console.error(err);
+                }
             }
+        })
+        .catch(error => {
+
         })
 }
