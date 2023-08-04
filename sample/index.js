d5d921121fc6813959f6e2ceb0c07acb6f9007b0
julypenguin <kaopenguin@gmail.com>
2023-08-04 16:14:26 +0800

    優化 - record 可以幫忙紀錄次數以及導入 catch()

--- sample/index.js
+++ sample/index.js
@@ -5,22 +5,29 @@ let repeat = process.argv[2] || 4
 repeat = Number(repeat)
 
 const _getAbout = apis.getAbout({ apiName: 'getAbout' })
+const _getAbout2 = apis.getAbout({ apiName: 'getAbout2' })
 
-const apisRecord = record(_getAbout)
-let resultIdx = 0
+const apisRecord = record(_getAbout, _getAbout2)
 
 for (let i = 1; i <= repeat; i++) {
     apisRecord.init(i)
     _getAbout
         .test()
         .then(({ res_time, res }) => {
-            resultIdx++
+            apisRecord.addTime(i, res_time) // 紀錄 api 回傳時間做加總
+            return _getAbout2.test()
+        })
+        .then(({ res_time, res }) => {
             apisRecord.finish(i, res_time)
-            if (resultIdx === repeat) {
+            if (apisRecord.idx === repeat) {
                 apisRecord.exportReport()
             }
         })
         .catch(error => {
+            apisRecord.finish(i, 0)
+            if (apisRecord.idx === repeat) {
+                apisRecord.exportReport()
+            }
             console.log('test_error', error)
         })
 }
